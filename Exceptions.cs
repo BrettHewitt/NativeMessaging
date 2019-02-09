99f9f501fd64c9460acd97bf655eac890b4e14ee
Brett Hewitt <brett_hewitt@live.co.uk>
2019-02-09 21:44:01 +0000

    Initial Commit

---
+++ Exceptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NativeMessaging
+{
+    [Serializable]
+    public class NotRegisteredWithChromeException : Exception
+    {
+        public NotRegisteredWithChromeException(string message) : base(message) { }
+    }
+}
